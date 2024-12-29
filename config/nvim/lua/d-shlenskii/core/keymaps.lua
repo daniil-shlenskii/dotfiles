b14b5bf70723711bb79a74a11be7d79d888d25cf
d-shlenskii <daniil.shlenskii@gmail.com>
2024-12-29 23:13:33 +0300

    nvim: unncecessary keymap removed

--- config/nvim/lua/d-shlenskii/core/keymaps.lua
+++ config/nvim/lua/d-shlenskii/core/keymaps.lua
@@ -40,10 +40,3 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
--- Text Blocks Shifting
-keymap.set("v", "<Tab>", ">", {})
-keymap.set("v", "<S-Tab>", "<", {})
-
-keymap.set("n", "<Tab>", "v>", {})
-keymap.set("n", "<S-Tab>", "v<", {})
