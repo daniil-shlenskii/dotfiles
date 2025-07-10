91550c07548f650ae1bb786cfdb1da7440bcafa6
d-shlenskii <daniil.shlenskii@gmail.com>
2025-07-10 21:45:23 +0300

    upd

--- config/nvim/lua/d-shlenskii/plugins/nvim-cmp.lua
+++ config/nvim/lua/d-shlenskii/plugins/nvim-cmp.lua
@@ -35,7 +35,6 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["\t"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
